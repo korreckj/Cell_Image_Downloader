3a2138ebbbfecc00c13b0f085519d2ff55a2d6d1
jeremiah korreck <korreckj328@gmail.com>
2025-12-30 21:31:24 -0500

    quick changes for downloading multiple queries with basic error handling.

--- main.py
+++ main.py
@@ -4,14 +4,15 @@
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 from bing_image_downloader import downloader
 
-query_strings=list("diff quik cocci")
-query_strings.append("diff quick rods")
-query_strings.append("diff quik yeast")
+query_strings= ["cytology diff quik cocci", "cytology diff quik rods", "cytology diff quik yeast"]
 
 def main():
     for query_string in query_strings:
-        downloader.download(query_string, limit=1000, output_dir="dataset")
-
+        try:
+            downloader.download(query_string, limit=100, output_dir="dataset")
+        except:
+            continue
+    # downloader.download("diff quick yeast", limit=1000, output_dir="dataset")
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
